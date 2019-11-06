c6edf037d85959b84483f9a3bbaa414e17e30fb0
pianoft <otanirobin1717a@gmail.com>
2019-11-06 19:23:00 +0900

    tester

---
+++ tester/tester2.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+r = requests.get('https://atcoder.jp/contests/abc144/tasks/abc144_f')
+soup = BeautifulSoup(r.text,"lxml")
+tags = soup.find_all('h3', text=lambda t: t and 'Sample' in t)
+i,j=1,1
+for tag in tags:
+    #print(tag.find_next_sibling().text)
+    #print("---------------------------------")
+    if i%2:
+        f = open("Sample Input %d" % j,"w+")
+    else:
+        f = open("Sample Output %d" % j, "w+")
+        j += 1
+    f.write(tag.find_next_sibling().text + "\n")
+    f.close()
+    i+=1
+
