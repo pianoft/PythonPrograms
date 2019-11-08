891fe3b06dec9509d298928620de0be420ff3997
pianoft <otanirobin1717a@gmail.com>
2019-11-08 20:23:51 +0900

    test

--- test/test/call.py
+++ test/test/call.py
@@ -1,12 +1,16 @@
 import os
 import tester2
-n = tester2.n
-myCmd='python main.py < In'
+m=int(input())
+n=tester2.n[m-1]
+c=tester2.a[m-1].capitalize()
+myCmd='python main.py < '+c+'in'
 a=[0 for i in range(0,n+1)]
 for i in range(1,n+1):
-    os.system(myCmd+str(i)+' > Submission'+str(i))
-    x1 = open("Out"+str(i),"r").read()
-    y1 = open("Submission"+str(i),"r").read()
+    s=c+'submission'+str(i)
+    print(s)
+    os.system(myCmd+str(i)+' > '+s)
+    x1 = open(c+"out"+str(i),"r").read()
+    y1 = open(c+"submission"+str(i),"r").read()
     a[i]= (x1==y1)
 if sum(a) < n:
     os.system("mpg123 " + ("wa.mp3") + " &")
@@ -16,9 +20,8 @@ else:
 for i in range(1,n+1):
     if a[i]==0:
         print("WA at Input"+str(i))
-        print(open("Out"+str(i),"r").read())
+        print(open(c+"out"+str(i),"r").read())
         print("---Below your output.")
-        open("Submission" + str(i), "r").read()
-
+        open(c+"submission" + str(i), "r").read()
     else:
         print("AC")
--- test/test/tester2.py
+++ test/test/tester2.py
@@ -1,17 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-r = requests.get('https://atcoder.jp/contests/abc144/tasks/abc144_b')
-soup = BeautifulSoup(r.text,"lxml")
-tags = soup.find_all('h3', text=lambda t: t and 'Sample' in t)
-i,j=1,1
-for tag in tags:
-    #print("---------------------------------")
-    if i%2:
-        f = open("In%d" % j,"w+")
-    else:
-        f = open("Out%d" % j, "w+")
-        j += 1
-    f.write(tag.find_next_sibling().text )
-    f.close()
-    i+=1
-n=int(len(tags)/2)
+a=['a','b','c','d','e','f']
+n=[-1 for i in range(6)]
+for i in range(6):
+    r = requests.get('https://atcoder.jp/contests/abc144/tasks/abc144_'+a[i])
+    soup = BeautifulSoup(r.text,"lxml")
+    tags = soup.find_all('h3', text=lambda t: t and 'Sample' in t)
+    k,j=1,1
+    print("length is "+str(len(tags)))
+    for tag in tags:
+        #print("---------------------------------")
+        if k%2:
+            f = open(a[i].capitalize()+"in%d" % j,"w+")
+        else:
+            f = open(a[i].capitalize()+"out%d" % j, "w+")
+            j += 1
+        f.write(tag.find_next_sibling().text )
+        f.close()
+        k += 1
+    n[i] = int(len(tags)/2)
