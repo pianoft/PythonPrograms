3bce553f6b59e62b2dc6890731dca7fa0e1d4f74
pianoft <otanirobin1717a@gmail.com>
2019-12-03 00:43:11 +0900

    test

--- test/test/freq1.py
+++ test/test/freq1.py
@@ -1,19 +1,17 @@
 from selenium import webdriver
-import time,os,cv2,requests,urllib
 import config
 options = webdriver.ChromeOptions()
 options.add_argument('--incognito')  # secret mode
 options.add_experimental_option('prefs', {'intl.accept_languages': 'en_US'}) # locale=en_US
 ##########################################################
-
 #from here
 driver2 = webdriver.Chrome(chrome_options=options)
 driver2.get(config.urlA)
-element = driver2.find_element_by_name('j_username')
-s=config.user_id
+element = driver2.find_element_by_name(config.tmp6)
+s=config.tmp1
 element.send_keys(s)
-element = driver2.find_element_by_name('j_password')
-element.send_keys(config.user_password)
+element = driver2.find_element_by_name(config.tmp7)
+element.send_keys(config.tmp2)
 element = driver2.find_element_by_id('btn')
 element.click()
 driver2.get(config.urlC)
@@ -22,10 +20,10 @@ driver2.get(config.urlC)
 driver1 = webdriver.Chrome(chrome_options=options)
 driver1.get(config.urlA)
 element = driver1.find_element_by_name("j_username")
-s=config.user_id
+s=config.tmp1
 element.send_keys(s)
 element = driver1.find_element_by_name('j_password')
-element.send_keys(config.user_password)
+element.send_keys(config.tmp2)
 element = driver1.find_element_by_id('btn')
 element.click()
 ##till here
