84dad6de9aa6a0d99737c846d90d7e9dba586560
pianoft <otanirobin1717a@gmail.com>
2019-12-03 16:05:09 +0900

    test

--- test/test/freq2.py
+++ test/test/freq2.py
@@ -6,9 +6,9 @@ options.add_argument('--incognito')  # secret mode
 options.add_experimental_option('prefs', {'intl.accept_languages': 'en_US'}) # locale=en_US
 driver = webdriver.Chrome(chrome_options=options)
 driver.get(config.tmp10)
-element = driver.find_element_by_class_name("js-username-field")
+element = driver.find_element_by_class_name(config.x1)
 element.send_keys(config.tmp9)
-element = driver.find_element_by_class_name("js-password-field")
+element = driver.find_element_by_class_name(config.x2)
 element.send_keys(config.tmp8)
 element.send_keys(Keys.ENTER)
-print("成功")
+print("成功2")
