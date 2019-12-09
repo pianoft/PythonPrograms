7d5e254bcea1154674b415b9904ca000defe1f9e
pianoft <otanirobin1717a@gmail.com>
2019-12-09 15:56:05 +0900

    test

--- test/test/sel.py
+++ test/test/sel.py
@@ -5,25 +5,25 @@ options = webdriver.ChromeOptions()
 options.add_argument('--incognito')  # secret mode
 options.add_experimental_option('prefs', {'intl.accept_languages': 'en_US'}) # locale=en_US
 ##########################################################
-driver1 = webdriver.Chrome(chrome_options=options)
-driver2 = webdriver.Chrome(chrome_options=options)
-driver1.get('https://atcoder.jp/contests/abc145/submissions/me')
-element = driver1.find_element_by_id("username")
-element.send_keys(config.USERNAME)
-element = driver1.find_element_by_id("password")
-element.send_keys(config.PASSWORD)
-element = driver1.find_element_by_id("submit")
-element.click()
-driver1.minimize_window()
+judgement_window = webdriver.Chrome(chrome_options=options)
+TargetProblemWindow = webdriver.Chrome(chrome_options=options)
+judgement_window.get('https://atcoder.jp/contests/abc147/submissions/me')
+elmnt = judgement_window.find_element_by_id("username")
+elmnt.send_keys(config.USERNAME)
+elmnt = judgement_window.find_element_by_id("password")
+elmnt.send_keys(config.PASSWORD)
+elmnt = judgement_window.find_element_by_id("submit")
+elmnt.click()
+judgement_window.minimize_window()
 ##########################################################################################
-driver2.get('https://atcoder.jp/contests/abc145/submissions/me')
-element = driver2.find_element_by_id("username")
-element.send_keys(config.USERNAME)
-element = driver2.find_element_by_id("password")
-element.send_keys(config.PASSWORD)
-element = driver2.find_element_by_id("submit")
-element.click()
-driver2.get('https://atcoder.jp/contests/abc145/tasks')
+TargetProblemWindow.get('https://atcoder.jp/contests/abc147/submissions/me')
+elmnt = TargetProblemWindow.find_element_by_id("username")
+elmnt.send_keys(config.USERNAME)
+elmnt = TargetProblemWindow.find_element_by_id("password")
+elmnt.send_keys(config.PASSWORD)
+elmnt = TargetProblemWindow.find_element_by_id("submit")
+elmnt.click()
+TargetProblemWindow.get('https://atcoder.jp/contests/abc147/tasks')
 ##########################################################3
 f = open("abc.txt", "w")
 f.write("0")
@@ -34,16 +34,16 @@ ac,wa,ce,tle,mle,=0,0,0,0,0
 sum=0
 def init():
     global ac, wa, tle, mle, sum, ce
-    s = driver1.page_source
+    s = judgement_window.page_source
     ac = s.count("Accepted")
     wa = s.count("Wrong Answer")
     ce = s.count("Compilation Error")
-    tle = s.count("Time limit Exceeded")
+    tle = s.count("Time Limit Exceeded")
     sum = ac+wa+ce+tle
     return
 def func():
     global ac,wa,tle,mle,sum,ce
-    s = driver1.page_source
+    s = judgement_window.page_source
     state = 0;
     if ac != s.count("Accepted"):
         print("AC")
@@ -57,7 +57,7 @@ def func():
         wa += 1
         os.system("mpg123 " + ("WA.mp3") + " &")
         state = 1;
-    if tle != s.count("Time limit Exceeded"):
+    if tle != s.count("Time Limit Exceeded"):
         print("TLE")
         tle += 1
         os.system("mpg123 " + ("TLE.mp3") + " &")
@@ -74,14 +74,13 @@ def func():
     return
 init()
 while 1:
-    driver1.refresh()
-    time.sleep(2)
-    func()
+    judgement_window.refresh()
+    time.sleep(0.5)
     f=open("tes.txt","w+")
-    s = driver2.current_url
+    s = TargetProblemWindow.current_url
     tmp = s.find('tasks/')
     f2 = open("abc.txt", "r").read()
-    time.sleep(1)
+    time.sleep(0.5)
     t = ""
     if tmp!=-1:
         length = (s.index("tasks"))
@@ -89,15 +88,20 @@ while 1:
             t += s[j]
         t += "submissions/me"
         url=t
+        if url != judgement_window.current_url:
+            print("変更あり　変更あり　変更あり　変更あり　変更あり　")
+            judgement_window.get(url)
+            init()
     if int(f2) == int(1):
         f2=open("abc.txt","w+")
         f2.write("0")
         f2.close()
-        element=driver2.find_element_by_id("input-open-file")
-        element.send_keys(os.getcwd()+"/main2.cpp")
-        element=driver2.find_element_by_id("submit")
-        element.click()
+        elmnt=TargetProblemWindow.find_element_by_id("input-open-file")
+        elmnt.send_keys(os.getcwd() + "/main2.cpp")
+        elmnt=TargetProblemWindow.find_element_by_id("submit")
+        elmnt.click()
     if tmp==-1:
         continue
-    f.write(driver2.current_url);
-    print(driver2.current_url)
+    func()
+    f.write(TargetProblemWindow.current_url)
+    print(TargetProblemWindow.current_url)
